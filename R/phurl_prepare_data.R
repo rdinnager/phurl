175555b93fd35336ef03dee8a9f3f42f35d9eb38
Russell Dinnage <r.dinnage@gmail.com>
2019-10-21 16:22:19 +1100

    added first function (phurl_prepare_data) and tests for it!

---
+++ R/phurl_prepare_data.R
@@ -0,0 +1,54 @@
+#' Prepare data for analysing with phurl models
+#'
+#' @param tree Phylogenetic tree of class \code{\link[ape]{phylo}} or \code{\link[tibble:tbl_df-class]{tibble}} 
+#' (e.g. using \code{\link[tidytree]{as_tibble}})
+#' @param traits Trait data. Can be a named \code{vector}, a \code{matrix} or \code{data.frame} with named rows, where the rownames
+#' are the species names, or a \code{data.frame} or \code{\link[tibble:tbl_df-class]{tibble}} with a column specifying the 
+#' species name. If species names are not referenced in either the rownames or a column, data will be matched 
+#' to the tree using order only (e.g. assuming data rows are in the same order as \code{tree$tip.label}).
+#' @param species_column Character value specifying which column of \code{traits} refers to the species 
+#' names, if they are not specified in the rownames. If not \code{NULL}, this will override any rownames in
+#' \code{traits} 
+#'
+#' @return A object of class \code{\link{phurl}}
+#' @export
+#'
+#' @examples
+phurl_prepare_data <- function(tree, traits, species_column = NULL) {
+  tree_df <- tidytree::as_tibble(tree)
+  tree <- tidytree::as.phylo(tree_df)
+  
+  if(!is.null(species_column)){
+    tree_df <- tree_df %>%
+      dplyr::left_join(traits, by = c("label" = species_column))
+  } else {
+    if(is.vector(traits)) {
+      if(is.null(names(traits))) {
+        trait_df <- dplyr::tibble(species = tree$tip.label,
+                                  trait = traits)
+        warning("traits object has no names, assuming same order as tree$tip.label")
+      } else {
+        trait_df <- dplyr::tibble(species = names(traits),
+                                  trait = traits)
+      }
+    } else {
+       if(!is.null(rownames(traits))) {
+        trait_df <- traits %>%
+          as.data.frame() %>%
+          tibble::rownames_to_column("species")
+       } else {
+         trait_df <- traits %>%
+           as.data.frame() %>%
+           dplyr::mutate(species = tree$tip.label) %>%
+           dplyr::select(species, dplyr::everything())
+          warning("traits object has no rownames, assuming same order as tree$tip.label")
+        }
+    }
+    tree_df <- tree_df %>%
+      dplyr::left_join(trait_df, by = c("label" = "species"))
+  }
+  
+  phurl_ob <- list(phylo = tree, tree_data = tree_df, model_fit = NULL, plots = NULL)
+  class(phurl_ob) <- "phurl"
+  phurl_ob
+}
---
+++ tests/testthat/test-phurl_prepare_data.R
@@ -0,0 +1,43 @@
+test_that("different classes of trait data can be loaded and are the same", {
+  tree <- RRphylo::DataOrnithodirans$treedino
+  traits_vec <- RRphylo::DataOrnithodirans$massdino
+  traits_vec_no_names <- unname(traits_vec)
+  traits_mat_no_names <- matrix(traits_vec, ncol = 1) 
+  colnames(traits_mat_no_names) <- "trait"
+  traits_mat_names <- traits_mat_no_names
+  rownames(traits_mat_names) <- names(traits_vec)
+  traits_dataframe_names <- as.data.frame(traits_mat_names)
+  traits_tibble_no_names_col <- traits_dataframe_names %>%
+    dplyr::mutate(species = names(traits_vec)) 
+  traits_dataframe_names_col <- traits_tibble_no_names_col %>%
+    as.data.frame()
+  
+  expect_warning(ob1 <- phurl_prepare_data(tree, traits_vec_no_names))
+  expect_warning(ob2 <- phurl_prepare_data(tree, traits_mat_no_names))
+  
+  expect_identical(ob1, ob2)
+  
+  ob_vec <- phurl_prepare_data(tree, traits_vec)
+  ob_mat <- phurl_prepare_data(tree, traits_mat_names)
+  ob_df_no_col <- phurl_prepare_data(tree, traits_dataframe_names)
+  ob_df_col <- phurl_prepare_data(tree, traits_dataframe_names_col, species_column = "species")
+  ob_tibble_col <- phurl_prepare_data(tree, traits_tibble_no_names_col, species_column = "species")
+  
+  expect_identical(ob_vec, ob_mat)
+  expect_identical(ob_vec, ob_df_no_col)
+  expect_identical(ob_vec, ob_df_col)
+  expect_identical(ob_vec, ob_tibble_col)
+  expect_identical(ob_mat, ob_df_no_col)
+  expect_identical(ob_mat, ob_df_col)
+  expect_identical(ob_mat, ob_tibble_col)
+  expect_identical(ob_df_no_col, ob_df_col)
+  expect_identical(ob_df_no_col, ob_tibble_col)
+  
+  expect_s3_class(ob_vec, "phurl")
+  
+  expect_null(ob_vec$model_fit)
+  expect_null(ob_vec$plots)
+  
+  
+  
+})
